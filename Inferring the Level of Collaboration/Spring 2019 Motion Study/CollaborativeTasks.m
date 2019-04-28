040b5c34b97fd0636a2dd6ac5a93ebc6905093ce
Nemlekar <hsnemlekar@wpi.edu>
2019-04-28 18:00:41 -0400

    minor plot change

--- Inferring the Level of Collaboration/Spring 2019 Motion Study/CollaborativeTasks.m
+++ Inferring the Level of Collaboration/Spring 2019 Motion Study/CollaborativeTasks.m
@@ -6,22 +6,22 @@ for i = 1:subj
     load(['Actual1/subject',num2str(i),'C.mat'])
     Human{i} = human;
     len = length(human.rw);
-    right = human.rw(:,1:3);
-    left = human.lw(:,1:3);
+    right = human.rw(:,[1,3,2]);
+    left = human.lw(:,[1,3,2]);
     o = quat2eul(human.o);
     
-    Right = smoothdata(right,'SmoothingFactor',0.05);
-    Left = smoothdata(left,'SmoothingFactor',0.05);
+    Right = smoothdata(right,1,'SmoothingFactor',0.05);
+    Left = smoothdata(left,1,'SmoothingFactor',0.05);
  
     fig = figure('Name','1','units','normalized','outerposition',[0 0 1 1]);
     hold on;
     grid on;
-    plot(1:len,Right(:,1),'b','Linewidth',2);
-    plot(1:len,Right(:,2),'g','Linewidth',2);
-    plot(1:len,Right(:,3),'c','Linewidth',2);
-    plot(1:len,Left(:,1),'b');
-    plot(1:len,Left(:,2),'g');
-    plot(1:len,Left(:,3),'c');
+    plot(1:len,Right(:,1),'b','Linewidth',1.5);
+    plot(1:len,Right(:,2),'g','Linewidth',1.5);
+    plot(1:len,Right(:,3),'r','Linewidth',1.5);
+    plot(1:len,Left(:,1),'b--','Linewidth',1.5);
+    plot(1:len,Left(:,2),'g--','Linewidth',1.5);
+    plot(1:len,Left(:,3),'r--','Linewidth',1.5)
     axis tight
     %print(fig, ['Plots/rw',num2str(i),'.png'],'-dpng','-r720');
     legend('X','Y','Z')
