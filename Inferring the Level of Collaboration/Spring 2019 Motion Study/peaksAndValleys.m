2fec48e04d5b1fec8be0363e1f90244cb3310815
Snehal Dikhale <43855981+SnehalD14@users.noreply.github.com>
2019-04-28 12:44:48 -0400

    Update peaksAndValleys.m

--- Inferring the Level of Collaboration/Spring 2019 Motion Study/peaksAndValleys.m
+++ Inferring the Level of Collaboration/Spring 2019 Motion Study/peaksAndValleys.m
@@ -3,7 +3,7 @@
 % finds the peaks and valleys of the plot.
 clear; close all; clc; 
 
-load('DataPilot1/subject1C'); % Load the subject data. C=Collaborative, NC=Non Collaborative
+load('Actual1/subject1C'); % Load the subject data. C=Collaborative, NC=Non Collaborative
 len = length(human.rw);
 x = zeros(1,len);
 y = zeros(1,len);
@@ -15,6 +15,12 @@ for i = 1:len
     z(1,i) = hand(2);
 
     % hand(1) = Subject data in the X-axis, hand(2) = Subject data in the Y-axis, hand(3) = Subject data in the Z-axis
+end
+
+len1 = length(human.o);
+o = zeros(len1,3);
+for i = 1:len1   
+    o(i,:) = quat2eul(human.o(i,:));
 end
 
 % Distance
@@ -40,19 +46,13 @@ normvel = normalize(vel);
 figure(1) %EUCLIDIAN DISTANCE PLOT
 index = 1:len;
 index1 = 1:(len-1);
-
 plot(index,dist)
 hold on
+%findpeaks(dist,'MinPeakDistance',1300,'Threshold',0.09,'MinPeakProminence',0.05)
+plot(index1,normvel) 
+legend("Distance","Velocity");
+title('Distance and Velocity')
 grid on
-findpeaks(dist,'MinPeakDistance',1300,'Threshold',0.09,'MinPeakProminence',0.05)
-title('Distance')
-figure(2)
-plot(index1,normvel)
-hold on
-yline(0)
-title('Velocity')
-grid on
-
 
 % [maxtabdist, mintabdist] = peakdet(dist, 0.25);
 % scatter(maxtabdist(:,1), maxtabdist(:,2), 150, 'r*');
@@ -66,49 +66,45 @@ grid on
 % end
 % disp(m);
 % plot(index1,m)
-figure(3) 
+
+figure(2) % (x,y,z) PLOT 
+x_sm = smoothdata(x,'SmoothingFactor',0.05);
+y_sm = smoothdata(y,'SmoothingFactor',0.05);
+z_sm = smoothdata(z,'SmoothingFactor',0.05);
+plot(index,x_sm,'m');
+hold on;
+plot(index,y_sm,'b');
+hold on;
+plot(index,z_sm,'k'); % plot a 2D graph of the subject data
+hold on;
+plot(index,dist)
 hold on
-plot(index,x,'m');
-hold on;
-plot(index,y,'b');
-hold on;
-plot(index,z,'k'); % plot a 2D graph of the subject data
-hold on;
 grid on ;
 title('Right Wrist Subject 1 - Collaborative Task');
 axis tight;
-[maxtabz, mintabz] = peakdet(z, 0.2);
-[maxtaby, mintaby] = peakdet(y, 0.1); % call the function that find the peaks and valleys.
-[maxtab, mintab] = peakdet(x, 0.1);
-scatter(mintabz(:,1), mintabz(:,2), 150, 'g*'); % plot the valleys of the subject data.
-scatter(maxtabz(:,1), maxtabz(:,2), 150, 'r*');
-scatter(mintaby(:,1), mintaby(:,2), 150, 'g*'); % plot the valleys of the subject data.
-scatter(maxtaby(:,1), maxtaby(:,2), 150, 'r*');
-scatter(mintab(:,1), mintab(:,2), 150, 'g*'); % plot the valleys of the subject data.
-scatter(maxtab(:,1), maxtab(:,2), 150, 'r*'); % plot the peaks of the subject data.
+legend('X','Y','Z','Distance');
+% [maxtabz, mintabz] = peakdet(z, 0.2);
+% [maxtaby, mintaby] = peakdet(y, 0.1); % call the function that find the peaks and valleys.
+% [maxtab, mintab] = peakdet(x, 0.1);
+% scatter(mintabz(:,1), mintabz(:,2), 150, 'g*'); % plot the valleys of the subject data.
+% scatter(maxtabz(:,1), maxtabz(:,2), 150, 'r*');
+% scatter(mintaby(:,1), mintaby(:,2), 150, 'g*'); % plot the valleys of the subject data.
+% scatter(maxtaby(:,1), maxtaby(:,2), 150, 'r*');
+% scatter(mintab(:,1), mintab(:,2), 150, 'g*'); % plot the valleys of the subject data.
+% scatter(maxtab(:,1), maxtab(:,2), 150, 'r*'); % plot the peaks of the subject data.
 
-% for i=1:18
-% xline(maxtab(i,1));
-% end
-% tolerance = 500;
-% tolerance2 = 1000; 
-% for i = 1:length(maxtab)
-%     for j= 1:length(maxtaby)
-%         for k=1:length(mintabz)
-%     if (abs(maxtab(i,1)-maxtaby(j,1))< tolerance)&& ((abs(maxtaby(j,1)-mintabz(k,1))< tolerance2)||(abs(maxtab(i,1)-mintabz(k,1))< tolerance2))
-%         disp('Peak x:')
-%         disp(maxtab(i,1));
-%         xline((maxtab(i,1)+maxtaby(j,1))/2);
-%         disp('Peak y:')
-%         disp(maxtaby(j,1))
-%         disp('Peak z:')
-%         disp(mintabz(k,1))
-%         if (abs(maxtab(i,1)-maxtaby(j,1))< tolerance)
-%             xline((maxtab(i,1)+maxtaby(j,1))/2);
-%     else if (abs(maxtabz(k,1)-maxtaby(j,1))< tolerance)&& ((abs(mintabz(k,1)-maxtab(i,1))< tolerance2)||(abs(maxtab(i,1)-maxtaby(j,1))< tolerance2))
-%         xline((mintabz(k,1)+maxtaby(j,1))/2);
-%         end
-%    end
-%        end  
- %   end
-%end
+figure(3) %ORIENTATION PLOT
+index_o=1:len1;
+Euler_x = smoothdata(o(:,1),'SmoothingFactor',0.1);
+Euler_y = smoothdata(o(:,2),'SmoothingFactor',0.1);
+Euler_z = smoothdata(o(:,3),'SmoothingFactor',0.1);
+plot(index_o,Euler_x);
+hold on
+plot(index_o,Euler_y);
+hold on
+plot(index_o,Euler_z);
+hold on
+title("Orientation Plot");
+legend("x","y","z");
+grid on
+
